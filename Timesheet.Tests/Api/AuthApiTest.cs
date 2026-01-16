d1ad611a35b34f8c3347cf952ffb959ecf23f180
SiobhanElson <48790556+SiobhanElson@users.noreply.github.com>
2026-01-16 09:59:54 +0000

    WIP packages and first test added

---
+++ Timesheet.Tests/Api/AuthApiTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RestAssured.Dsl;
+using System.Net;
+
+namespace Timesheet.Tests.Api;
+
+
+{
+    public class AuthAPITest
+    {
+
+        [Test]
+        public void TestLoginReturnsPositiveResponse()
+        {
+            Login login = new("admin@test.com", "password123");
+
+            HttpResponseMessage response = Given()
+                                .Body(login)
+                                .ContentType("application/json")
+                                .Post("http://localhost:8080/v1/auth/login")
+                                .Then()
+                                .Extract()
+                                .Response();
+        }
+
+    }
+}
---
+++ Timesheet.Tests/Api/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timesheet.Tests.Api
+{
+    public class Login
+    {
+        public string email { get; set; } = string.Empty;
+
+        public string password { get; set; } = string.Empty;
+
+        public Login(string email, string password)
+        {
+            this.email = email;
+            this.password = password;
+        }
+    }
+}
